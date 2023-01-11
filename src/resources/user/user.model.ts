1a8bed6b5097399cacab31f7de13b459edcd0fed
Darryl Mangibin <darrylmangibin@gmail.com>
2023-01-11 14:37:10 +0800

    Hashed password after saving

--- src/resources/user/user.model.ts
+++ src/resources/user/user.model.ts
@@ -1,3 +1,4 @@
+import hashPassword from '@src/utils/password/hash.password';
 import { model, PaginateModel } from 'mongoose';
 import paginate from 'mongoose-paginate-v2';
 import { UserData, UserDocument } from './user.interface';
@@ -5,6 +6,14 @@ import { UserData, UserDocument } from './user.interface';
 import UserSchema from './user.schema';
 
 UserSchema.plugin(paginate);
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = await hashPassword(this.password);
+});
 
 const User = model<UserData, PaginateModel<UserDocument>>('User', UserSchema);
 
