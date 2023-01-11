95fc9fbbbedab87c32b6487f84163f94dbe93908
Darryl Mangibin <darrylmangibin@gmail.com>
2023-01-11 13:32:47 +0800

    Initialize middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
-import express, { Express } from 'express';
+import express, { Express, RequestHandler } from 'express';
 import 'colors';
 import 'dotenv/config';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import morgan from 'morgan';
 
 import connectDatabase from '@utils/database/connect.database';
 
 class App {
   public app: Express = express();
 
-  constructor(private port: number, routes: AppRoutes[]) {}
+  constructor(private port: number, routes: AppRoutes[]) {
+    this.initializeMiddleware(cors(), helmet(), compression(), morgan('dev'));
+  }
 
   public listen() {
     this.app.listen(this.port, async () => {
@@ -20,6 +26,15 @@ class App {
   private initializeDatabaseConnection = async () => {
     await connectDatabase('MongoDB connected...');
   };
+
+  private initializeMiddleware(...handlers: RequestHandler[]) {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+
+    handlers.forEach(handler => {
+      this.app.use(handler);
+    });
+  }
 }
 
 export default App;
